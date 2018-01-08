05ed03681ad79e04096a2b59513a0ab9901dd251
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-01-08 01:45:14 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,3 +6,7 @@ def sort_array_desc(integers)
   #integers.sort_by { |i| i }.reverse
   integers.sort_by { |i| -i }
 end
+
+def sort_array_char_count(integers)
+  integers.sort_by { |i| i >=1  }
+end
